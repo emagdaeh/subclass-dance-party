7f80c489bca05069def8cfeedc9469cf05cf4037
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-13 12:00:09 -0700

    Save before lunch, working on line up and dancer specific activity

--- src/init.js
+++ src/init.js
@@ -23,11 +23,31 @@ $(document).ready(function() {
     // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
-      $('body').height() * Math.random(),
-      $('body').width() * Math.random(),
+      $('danceFloor').height() * Math.random(),
+      $('danceFloor').width() * Math.random(),
       Math.random() * 1000
     );
-    $('body').append(dancer.$node);
+    $('.danceFloor').append(dancer.$node);
   });
 });
+
+/*
+function show_image(src, width, height, alt) {
+  var img = document.createElement("img");
+  img.src = src;
+  img.width = width;
+  img.height = height;
+  img.alt = alt;
+
+  // set the position
+  img.style.position = 'absolute';
+  img.style.top = document.body.clientHeight * Math.random() + 'px';
+  img.style.left = document.body.clientWidth * Math.random() + 'px';
+
+  document.body.appendChild(img);
+}
+document.getElementById('foo').addEventListener('click', () =>
+  show_image("http://placekitten.com/200/300", 200, 300, 'foo')
+);
+*/
 
--- src/popDancer.js
+++ src/popDancer.js
@@ -12,9 +12,25 @@ MakePopDancer.prototype.step = function() {
   MakeDancer.prototype.step.call(this);
 
   this.$node.toggle();
-  // setTimeout(function() {
-  //   step.call(MakeDancer.prototype);
-  //   //this.$node.toggle();
-  // }, timeBetweenSteps);
 
 };
+
+/*
+function show_image(src, width, height, alt) {
+  var img = document.createElement("img");
+  img.src = src;
+  img.width = width;
+  img.height = height;
+  img.alt = alt;
+
+  // set the position
+  img.style.position = 'absolute';
+  img.style.top = document.body.clientHeight * Math.random() + 'px';
+  img.style.left = document.body.clientWidth * Math.random() + 'px';
+
+  document.body.appendChild(img);
+}
+document.getElementById('foo').addEventListener('click', () =>
+  show_image("http://placekitten.com/200/300", 200, 300, 'foo')
+);
+*/
